ddd3c3ac8fab2e8b17f72518ac7c992b40fee2ab
Faraz Fallahi <fffaraz@gmail.com>
2014-02-14 22:14:09 +0330

    UI design

--- Toefl-Speaking-Recorder/mainwindow.cpp
+++ Toefl-Speaking-Recorder/mainwindow.cpp
@@ -6,9 +6,32 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    isStarted = false;
+    ui->btnStart->setFocus();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QMessageBox::aboutQt(this);
+}
+
+void MainWindow::on_btnStart_clicked()
+{
+    if(!isStarted)
+    {
+        isStarted = true;
+        ui->btnSkip->setEnabled(true);
+        ui->btnStart->setText("Stop");
+    }
+    else
+    {
+        isStarted = false;
+        ui->btnSkip->setEnabled(false);
+        ui->btnStart->setText("Start");
+    }
 }
--- Toefl-Speaking-Recorder/mainwindow.h
+++ Toefl-Speaking-Recorder/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtCore>
+#include <QMessageBox>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_actionAbout_triggered();
+
+    void on_btnStart_clicked();
+
 private:
     Ui::MainWindow *ui;
+    bool isStarted;
 };
 
 #endif // MAINWINDOW_H
